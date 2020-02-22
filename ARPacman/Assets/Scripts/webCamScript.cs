393e7487a437b0877443653ef6657375747d51a5
rshaw02 <rshaw@student.neumont.edu>
2020-02-21 18:15:56 -0700

    Added root unity project, scene contains plane for using camera, but the gyroscope is wrong.

---
+++ ARPacman/Assets/Scripts/webCamScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webCamScript : MonoBehaviour
+{
+    public GameObject webCameraPlane;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            GameObject cameraParent = new GameObject("camParent");
+            cameraParent.transform.position = transform.position;
+            transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right, 90);
+        }
+        Input.gyro.enabled = true;
+
+        WebCamTexture webCamTexture = new WebCamTexture();
+        webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        transform.localRotation = cameraRotation;
+    }
+}
