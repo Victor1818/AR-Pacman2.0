92140604cfab82383efe5ca6344c013535520fa5
rshaw02 <rshaw@student.neumont.edu>
2020-02-27 16:30:30 -0700

    Camera rotation corrected for portrait

--- ARPacman/Assets/Scripts/webCamScript.cs
+++ ARPacman/Assets/Scripts/webCamScript.cs
@@ -16,7 +16,7 @@ public class webCamScript : MonoBehaviour
             GameObject cameraParent = new GameObject("camParent");
             cameraParent.transform.position = transform.position;
             transform.parent = cameraParent.transform;
-            cameraParent.transform.Rotate(Vector3.right, 90);
+            cameraParent.transform.Rotate(Vector3.right, 90f);
         }
         Input.gyro.enabled = true;
 
