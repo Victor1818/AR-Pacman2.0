bff0d69cb2822fdf477c388ae88da8a4a226f303
Victor <Victor_Ramos_001@hotmail.com>
2020-02-27 19:52:58 -0700

    !

---
+++ ARPacman/Assets/Scripts/ghost_Clyde.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Will be used for all the Ghosts unless we want the Ghosts to have their exact AI like in the original
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
