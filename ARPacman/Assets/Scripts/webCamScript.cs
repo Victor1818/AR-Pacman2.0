fbd9e5eca2a1dc7620d338dfcb68b9f300e96132
rshaw02 <rshaw@student.neumont.edu>
2020-02-28 02:02:19 -0700

    Started working on moving using GPS, not working.

--- ARPacman/Assets/Scripts/webCamScript.cs
+++ ARPacman/Assets/Scripts/webCamScript.cs
@@ -9,17 +9,24 @@ public class webCamScript : MonoBehaviour
     public GameObject webCameraPlane;
 
     private LocationService locationService;
+    LocationInfo previousLocationInfo;
 
     // Start is called before the first frame update
     void Start()
     {
         locationService = new LocationService();
+        locationService.Start(0.1f);
         if (Application.isMobilePlatform)
         {
             GameObject cameraParent = new GameObject("camParent");
             cameraParent.transform.position = transform.position;
             transform.parent = cameraParent.transform;
             cameraParent.transform.Rotate(Vector3.right, 90f);
+        }
+        if(locationService.status == LocationServiceStatus.Running)
+        {
+            previousLocationInfo = new LocationInfo();
+            previousLocationInfo = locationService.lastData;
         }
         Input.gyro.enabled = true;
 
@@ -31,6 +38,8 @@ public class webCamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(new Vector3(previousLocationInfo.longitude - locationService.lastData.longitude, previousLocationInfo.latitude - locationService.lastData.latitude));
+        previousLocationInfo = locationService.lastData;
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
         transform.localRotation = cameraRotation;
     }
